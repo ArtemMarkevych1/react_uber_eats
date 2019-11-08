5b4f50961f27768b6d563d8d14b556c058d56804
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-11-08 14:38:36 +0200

    Updates

---
+++ precache-manifest.f1b4a02eda94f6e3964b6a440a9ff390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6963ad6c8ab88a5d5f06a3530b84e0d",
+    "url": "/react_uber_eats/index.html"
+  },
+  {
+    "revision": "ec5d1a53ce2d8b7fb461",
+    "url": "/react_uber_eats/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6a0acefefdec447f36b0",
+    "url": "/react_uber_eats/static/js/2.49b3a511.chunk.js"
+  },
+  {
+    "revision": "ec5d1a53ce2d8b7fb461",
+    "url": "/react_uber_eats/static/js/main.a7058eb6.chunk.js"
+  },
+  {
+    "revision": "61246acc1f2e51430f24",
+    "url": "/react_uber_eats/static/js/runtime-main.ac1157f5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react_uber_eats/static/media/logo.25bf045c.svg"
+  }
+]);
